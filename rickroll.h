f70d3ec837c50d8ae80e2428e71b357597ec8c4e
Rahul Mishra <61725851+Rahul-Mishra69@users.noreply.github.com>
2020-10-24 20:31:57 +0530

    Add files via upload

---
+++ rickroll.h
@@ -0,0 +1,9 @@
+void rickroll
+{
+    printf("Never gonna give you up\n");
+    printf("Never gonna let you down\n");
+    printf("Never gonna run around and desert you\n");
+    printf("Never gonna make you cry\n");
+    printf("Never gonna say goodbye\n");
+    printf("Never gonna tell a lie and hurt you\n");
+}
